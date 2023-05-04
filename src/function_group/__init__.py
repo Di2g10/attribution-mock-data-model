9b198dbc90e773a36026e259675b99a2c028b07f
David Irvine <Dirvine@clever-touch.com>
2023-05-04 13:56:05 +0100

    Add basic folders

---
+++ src/function_group/__init__.py
@@ -0,0 +1,1 @@
+# where you might put code that should be run for any of the functions within this folder.
---
+++ src/main.py
@@ -0,0 +1,11 @@
+import function_group.example as example_funcs
+
+
+def main():
+    # Example calling a function
+    print(example_funcs.example(10))
+
+
+if __name__ == '__main__':
+    main()
+
