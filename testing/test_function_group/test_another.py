f752db9a917fedf393294fc05b00a6cffed07b8c
David Irvine <Dirvine@clever-touch.com>
2023-06-20 14:38:18 +0100

    # trigger ruff check

--- testing/test_function_group/test_another.py
+++ testing/test_function_group/test_another.py
@@ -1,10 +1,16 @@
+"""Needs Updating."""
 import unittest
 
 
 class MyTestCase(unittest.TestCase):
+
+    """Teeds Updating."""
+
     def test_something(self):
+        """Needs Updating."""
         self.assertEqual(True, False)  # add assertion here
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
+    # trigger ruff check
