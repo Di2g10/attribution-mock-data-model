0ddaf499dcf22102e629c319b47854ec1a9b14c6
David Irvine <Dirvine@clever-touch.com>
2023-06-20 12:27:18 +0100

    Test Black Commit hook

--- src/main.py
+++ src/main.py
@@ -6,6 +6,6 @@ def main():
     print(example_funcs.example(10))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    "Test to see if black will reduce this really long string into something more readable on multiple lines, So my rambles can be read more easily, more quickly, and more efficiently. blah blah blah blah bla"
     main()
-
