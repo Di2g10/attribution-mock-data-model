68a79981dba7e2d9601fe3258853dae435f119e8
David Irvine <Dirvine@clever-touch.com>
2023-06-20 14:55:15 +0100

    # Configure Ruff Settings and Amend project to match configured rules.

--- src/function_group/example.py
+++ src/function_group/example.py
@@ -1,11 +1,11 @@
+"""Contains some example code for the project."""
 
 
 def example(x: int) -> int:
-    """
-    Description of the function purpose goes here. This function does nothing but demonstrated how functions should be
-    written
+    """Add one to the input.
+
     :param x: input expected to be any integer
-    :return: x expected to be an integer
+    :return: x expected to be an integer.
     """
     x = x + 1
     return x
@@ -14,9 +14,10 @@ def example(x: int) -> int:
 def _main():
     # example demonstrating function
     x = "Some input"
-    print(f'input: {x} => {example(x)}')
+    print(f"input: {x} => {example(x)}")
 
 
-if __name__ == '__main__':
-    # To ensure this code is only run when this module is directly run. And now executed when module is imported
+if __name__ == "__main__":
+    # To ensure this code is only run when this module is directly run.
+    # And not executed when module is imported
     _main()
