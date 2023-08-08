0e211621bea9b2d2981a246a63f2b9c55dfb12a7
Anne Young <ayoung@clever-touch.com>
2023-08-08 17:59:41 +0100

    Script updated for file manipulation before upload. 08/08/2023 ALY

--- src/prep_main.py
+++ src/prep_main.py
@@ -2,13 +2,11 @@ import pandas as pd
 pd.set_option('display.max_columns', None)
 import filepaths as fpath
 from datetime import date
-from upload_data_creation import rename_fields_actc, fix_date, select_records, remove_tm,create_additional_fields
+from upload_data_creation import rename_fields, fix_date, select_records, remove_tm,create_additional_fields
 
-# Read in data
-actc_df = pd.read_excel(fpath.workspace_directory / 'ACTC Course Registration - July 2022 to March 2023.xlsx', sheet_name='Sheet1')
-
-# Manipulate it for upload
-renamed_actc_df = rename_fields_actc(actc_df)
-fixed_actc_df = fix_date(renamed_actc_df,"%d-%m-%Y")
-records_actc_df = select_records(fixed_actc_df)
-removed_tm_actc_df = remove_tm(records_actc_df)
+# Manipulate for upload
+if __name__ == '__main__':
+    renamed_actc_df = rename_fields(actc_df)
+    fixed_actc_df = fix_date(renamed_actc_df, "%d-%m-%Y")
+    records_actc_df = select_records(fixed_actc_df)
+    removed_tm_actc_df = remove_tm(records_actc_df)
--- src/upload_data_creation.py
+++ src/upload_data_creation.py
@@ -2,44 +2,104 @@ import pandas as pd
 pd.set_option('display.max_columns', None)
 import filepaths as fpath
 from datetime import date
+from pathlib import Path
 
 
-def rename_fields_actc(df: pd.DataFrame):
+def import_upload_data():
     """
-    Rename fields in the ACTC file to match Marketo API, apart from the list name which is carried out in a different function
+    Import the three upload files
+    """
+    actc_df = pd.read_excel(fpath.workspace_directory / 'ACTC Course Registration - July 2022 to March 2023.xlsx',
+                            sheet_name='Sheet1')
+    pi_df = pd.read_excel(fpath.workspace_directory / 'PI Course Registration Information - Dec 2022 to Feb 2023.xlsx',
+                          sheet_name='Summary')
+    training_df = pd.read_excel(fpath.workspace_directory / 'Training Manager_January_2018 to February 2023.xlsx',
+                                sheet_name='2018-2022 Sept')
+
+def import_pl_mapping_file(campaign: str):
+    """
+    Import mapping file for the program/list name based on campaign and the field mapping based on campaign
+    """
+    file_name = 'Aveva API Import ID_s.xlsx'
+    if campaign=='ACTC':
+        prog_list_df = pd.read_excel(fpath.workspace_directory / file_name,
+                                     sheet_name='ALL IDs CT')
+        print('ACTC "prog_list_df" created')
+    elif campaign=='PI':
+        prog_list_df = pd.read_excel(fpath.workspace_directory / file_name,
+                                     sheet_name='ALL IDs PI')
+        print('PI "prog_list_df" created')
+    elif campaign=='Training':
+        prog_list_df = pd.read_excel(fpath.workspace_directory / file_name,
+                                     sheet_name='ALL IDs TM')
+    elif print('Campaign unsupported'):
+
+    return prog_list_df
+
+
+def import_field_mapping_file(campaign: str):
+    """
+    Import mapping file for the field names to be changed to align to Marketo, based on campaign.
+    campaign: client campaign. Choice of 'ACTC', 'PI' or 'Training'
+    """
+    file_name = 'Field Mapping_V1.xlsx'
+    if campaign=='ACTC':
+        field_mapping_df = pd.read_excel(fpath.workspace_directory / file_name,
+                                     sheet_name='ALL IDs CT')
+        # Select rows where Campaign File = ACTC
+        print('ACTC "prog_list_df" created')
+    elif campaign=='PI':
+        field_mapping_df = pd.read_excel(fpath.workspace_directory / file_name,
+                                     sheet_name='ALL IDs PI')
+        print('PI "prog_list_df" created')
+    elif campaign=='Training':
+        field_mapping_df = pd.read_excel(fpath.workspace_directory / file_name,
+                                     sheet_name='ALL IDs TM')
+        print('Training "prog_list_df" created')
+    elif print('Campaign unsupported'):
+
+    return field_mapping_df
+
+
+def rename_fields(df: pd.DataFrame,
+                  campaign: str,
+                  field_mapping_doc: Path,
+                  sheet_name:str):
+    """
+    Rename fields in the dataframe to match Marketo API, apart from the list name which is carried out in a different function
+
+    ENSURE THAT THE PROGRAM AND LIST FIELDS ARE NAMED CORRECTLY HERE FOR THE PROGRAM_VALID FUNCTION
+
     df: File from client event with leads in, which are to be created/updated in Marketo
-    CHECK WHICH FIELDS ARE GOING INTO MARKETO
+    campaign: client campaign. Choice of 'ACTC', 'PI' or 'Training'
+    field_mapping_doc: the path where the field mapping document in, with the 'Field in Upload File' and 'REST API Name' name
+    sheet_name: sheet name to be imported of the field mapping doc
     """
-    df = df.rename(columns={'EMAIL Address ': 'email',
-                            'FirstName': 'firstName',
-                            'LastName': 'lastName',
-                            'institution': 'Institution', #Check field in Marketo
-                            'city': 'city', #Check field in Marketo, city or City__c
-                            'country': 'country code', #Check field in Marketo, this is meant to be Country Code
-                            'Country Name': 'country', #Check field in Marketo, country or Country__c or LL_Country__c
-                            'category': 'Category', #Check field in Marketo
-                            'department': 'department', #Check field in Marketo, department or engagio__Department__c
-                            'Parent ID': 'Parent_Acct__c.', #Check field in Marketo
-                            'Part Number': 'List Name part one',
-                            'Course': 'List Name part two',
-                            'Product': 'product',
-                            'New Product Name': 'New Product Name', #Check field in Marketo
-                            'Course Category': 'Course Category', #Check field in Marketo
-                            'Solution': 'Solution', #Check field in Marketo
-                            'enrolled': 'pmi_MCL_Date__c'})
-    #print(df.info())
+    field_mapping = pd.read_excel(field_mapping_doc,
+                                  sheet_name=sheet_name)
+    field_mapping = field_mapping[field_mapping['Campaign Field'] == campaign]
+    field_mapping_dict = {}
+    for index, row in field_mapping.iterrows():
+        old_name = row['Field in Upload File']
+        new_name = row['REST API Name']
+        dict_item = {old_name, new_name}
+        field_mapping_dict.update(dict_item)
+    df.rename(columns=field_mapping_dict)
     return df
 
 
-def fix_date(df: pd.DataFrame, desired_format: date):
+def fix_date(df: pd.DataFrame,
+             desired_format: date):
     """
     Fix the date field
     df: dataframe with leads in with the fields named to match Marketo API
     desired_format: The format that the date is supposed to be in
+
     CHECK DATE FORMAT FOR MARKETO
+
     """
     df['pmi_MCL_Date__c'] = pd.to_datetime(df['pmi_MCL_Date__c'], format=desired_format, errors='coerce')
-    return(df)
+    return df
 
 
 def select_records(df: pd.DataFrame):
@@ -56,22 +116,25 @@ def select_records(df: pd.DataFrame):
     return(df)
 
 
-def program_valid(df: pd.DataFrame, client_campaign_name: str):
+def program_valid(df: pd.DataFrame,
+                  campaign: str,
+                  prog_list_df: pd.DataFrame):
     """
     Ensures the program names are valid; that they're programs in Marketo
     df: dataframe with leads in with the fields renamed
-    client_campaign_name: The campaign name as called by the client. One of three:
-    'ACTC', 'PI' or 'Training'
+    campaign: The campaign name as called by the client. One of three: 'ACTC', 'PI' or 'Training'
+    prog_list_df: the valid program and list values.
+
+    PROG_LIST_DF SHOULD REFER TO THE DF RETURNED FROM THE import_pl_mapping_file FUNCTION
+
     """
-    #print('original',df)
-    #WHEN FOLDER NAME = ACTC
-    program_list_df = pd.read_excel(fpath.workspace_directory / 'Aveva API Import ID_s.xlsx',
-                            sheet_name='ALL IDs CT')
+
     # Remove TM superscript
     search_symbol = '\u2122'
     df['List Name part two'] = df['List Name part two'].str.replace(search_symbol, '')
-    #print('fixed',df)
-    return(df)
+
+
+    return df
 
 
 def create_additional_fields(df: pd.DataFrame, program_name: str):
@@ -84,7 +147,6 @@ def create_additional_fields(df: pd.DataFrame, program_name: str):
     df['nonmarketable'] = 'True'
     df['Dynamic_Lead_Source__c'] = 'AVEVA Virtual Event'
     df['pmi_Original_Lead_Source__c'] = 'AVEVA Virtual Event' # Check correct Marketo field
-    df['Dynamic_Detailed_Lead_Source__c'] = ''
 
 
 
